e70a1655873d7ba3b004108f9f679713fd65299d
Ogefere Ogheneruno <89848528+Ogheneruno@users.noreply.github.com>
2023-07-14 02:58:39 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	int intT;
 	float floatT;
 	long longIntT;
-	long long int lLIntT;
+	long long int lLIT;
 	char charT;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charT));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intT));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longIntT));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lLIntT));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lLIT));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatT));
 
 	return (0);
