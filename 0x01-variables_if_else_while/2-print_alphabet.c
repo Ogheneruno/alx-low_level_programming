1c759f164856617b8b33308183f7ac2369f5ea94
Ogefere Ogheneruno <89848528+Ogheneruno@users.noreply.github.com>
2023-07-15 03:16:49 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,8 +13,8 @@ int main(void)
 	char lowerCase;
 
 	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
-		putchar(lowerCase);
-		putchar('\n');
+		{putchar(lowerCase);
+		putchar('\n');}
 
 	return (0);
 }
